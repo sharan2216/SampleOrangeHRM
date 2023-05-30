b231f7229d5e654d930ed93e23bc67f320771dab
sharan66 <sksharan666@gmail.com>
2023-05-30 16:18:44 +0530

    fibonacci recursion method added

--- operations/test_fibonacci.py
+++ operations/test_fibonacci.py
@@ -15,19 +15,37 @@
 
 # 2nd way using For loop in python :----------
 
-def fibonacci2(n):
+# def fibonacci2(n):
+#
+#     n1,n2 = 0,1
+#     if n==1:
+#         print(n)
+#     else:
+#         print(n1)
+#         print(n2)
+#     for i in range(2, n):
+#         sum = n1 + n2
+#         print(sum)
+#         n1 = n2
+#         n2 = sum
+#
+#
+# fibonacci2(10)
 
-    n1,n2 = 0,1
-    if n==1:
-        print(n)
+#using Recursion method:--------
+def fibonacci_rec(n):
+    if n<=1:
+        return n
     else:
-        print(n1)
-        print(n2)
-    for i in range(2, n):
-        sum = n1 + n2
-        print(sum)
-        n1 = n2
-        n2 = sum
+        return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
-fibonacci2(10)
+    n=10
+    if n<=0:
+        print("Invalid scenario")
+    else:
+        for i in range(n):
+            print(fibonacci_rec(i))
+
+
+fibonacci_rec(10)
